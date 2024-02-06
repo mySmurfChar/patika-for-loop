2dd6fb3a10f8d9768614203d4329035ac0d13d6d
MuhammetCanLikya <158784308+mySmurfChar@users.noreply.github.com>
2024-02-06 04:52:05 +0300

    first commit

---
+++ patika-for-loop-break/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace patika_for_loop_break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.WriteLine("Enter number of rows: ");
+            n=int.Parse(Console.ReadLine());
+            for (i = n; i >= 1; i--)
+            {
+                for (j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                int t = n - i;
+                for (; t >= 0; t--)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+
+        }
+
+
+
+        }
+    }
+
